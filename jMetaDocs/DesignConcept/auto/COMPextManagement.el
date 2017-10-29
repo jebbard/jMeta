0bdc8ab14be2ae7a5a673fc0769010232650bee2
(no author) <(no author)@a7d6292f-cb60-4ff3-81e5-f08f5b053fae>
2017-10-29 08:33:28 +0000

    Simplified Design document
    Former-commit-id: 1d0207a114ffd4ba9ac55f96d3e238cb2c3152eb

--- jMetaDocs/DesignConcept/auto/COMPextManagement.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook
- "COMPextManagement"
- (lambda ()
-   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
-   (LaTeX-add-labels
-    "sec:COMPextensionManagementDesign")))
-
