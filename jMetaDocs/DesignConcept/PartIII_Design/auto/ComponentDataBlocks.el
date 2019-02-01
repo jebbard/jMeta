97bd38f959b2a5f57a5157671a8318824d4b58c1
(no author) <(no author)@a7d6292f-cb60-4ff3-81e5-f08f5b053fae>
2019-02-01 16:20:09 +0000

    DataBlocks component refactoring and design: First quick wins
    Former-commit-id: 0d495954eb31bd28cabea1cb92d4c65536f9e4a1

---
+++ jMetaDocs/DesignConcept/PartIII_Design/auto/ComponentDataBlocks.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "ComponentDataBlocks"
+ (lambda ()
+   (LaTeX-add-labels
+    "sec:COMPdataBlockManagementDesign"
+    "sec:BasicConceptforReading"
+    "sec:ReadingSteps"
+    "sec:FieldParsing"
+    "sec:BackwardReading"
+    "sec:APIDesign"))
+ :latex)
+
